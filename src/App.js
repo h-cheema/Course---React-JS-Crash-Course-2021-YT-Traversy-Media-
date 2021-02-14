b0ab74bf1a407df13cfea96c98f7fcb1dfd88d72
h-cheema <harjinder.sc96@gmail.com>
2021-02-13 23:00:52 -0500

    reset repo

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Header from './components/Header'
+
+// function App() {
+//   const name = 'Harj'
+//   const x = false
+//   return (
+//     <div className='App'>
+//       <h1>Hello From React</h1>
+//       <h1>Hello {name}</h1>
+//       <h1>{x ? 'Yes' : 'No'}</h1>
+//     </div>
+//   );
+// }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class.</h1>
+//   }
+// }
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Header 
+      // title={}
+      />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+const tasks = []
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
+ 
