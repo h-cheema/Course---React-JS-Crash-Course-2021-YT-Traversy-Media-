f88a1c5fa099cda677c9b752868f5654a51b70bd
h-cheema <harjinder.sc96@gmail.com>
2021-02-14 21:49:24 -0500

    finished toggle reminder and conditional styling section

--- src/App.js
+++ src/App.js
@@ -32,10 +32,24 @@ const App = () => {
   }
 
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? {...task, reminder :
+          !task.reminder 
+        } : task
+      )
+    )
+  }
+
+
   return (
     <div className='container'>
       <Header />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks To Show') }
+      {tasks.length > 0 ? 
+        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')
+      }
     </div>
   )
 }
