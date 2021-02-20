34c9f5c9b3a8143b761667f811da53e3db093e25
h-cheema <harjinder.sc96@gmail.com>
2021-02-19 21:30:00 -0500

    finished sections 1 - 4

---
+++ Javascript/JavaScript ES6, ES7, ES8 (freeCodeCamp.org )/2. template literals/index.js
@@ -0,0 +1,12 @@
+let word1 = 'harj';
+let word2 = 'jin';
+
+// const fullName = `${word1} ${word2}`;
+
+// let example = 'Hello \n ' + 'world!';
+let example = `${word1} ${word2}`
+
+
+// console.log(fullName);
+console.log(example)
+// document.getElementById('example').innerText = example;
