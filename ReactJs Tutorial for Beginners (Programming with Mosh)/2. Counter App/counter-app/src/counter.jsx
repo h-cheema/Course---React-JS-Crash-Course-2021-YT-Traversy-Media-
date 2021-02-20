84c2a5db79880e725f2d90605bbdc2956356410b
h-cheema <harjinder.sc96@gmail.com>
2021-02-19 19:36:32 -0500

    finished section: Passing Event Arguments (1:17:31)

--- ReactJs Tutorial for Beginners (Programming with Mosh)/2. Counter App/counter-app/src/counter.jsx
+++ ReactJs Tutorial for Beginners (Programming with Mosh)/2. Counter App/counter-app/src/counter.jsx
@@ -3,29 +3,52 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 1,
-        imageUrl: 'https://picsum.photos/200'
+        count: 0,
+        // tags: ["tag1", "tag2", "tag3"]
     };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = (product) => {
+        // console.log('Increment Clicked', this)
+        console.log(product)
+        this.state.count++;
+        this.setState({ count: this.state.count + 1 })
+    }
 
     styles = {
         fontSize: 10,
         fontWeight: 'bold'
     };
 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>; // Empty arrays
+        { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+    }
+
     render() { 
-
-        const classes = "badge m-2 ";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-
         return (
             <div>
-                <img src={this.state.imageUrl}/>
-                <span className={classes} style={this.styles}>
-                    {this.formatCount()}
-                </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={() => this.handleIncrement(product)}
+                    className={}>Increment</button>
+                {/* { this.state.tags.length === 0 && "Please create a new tag!" } */}
+                {/* <ul>
+                    {this.renderTags()}
+                </ul> */}
+
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        const classes = "badge m-2 ";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
