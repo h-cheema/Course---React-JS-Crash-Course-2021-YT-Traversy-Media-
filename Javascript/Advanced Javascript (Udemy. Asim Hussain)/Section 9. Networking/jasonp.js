ec958362d213bd2a2cd90da4c26e51e6bdf7bf7a
h-cheema <harjinder.sc96@gmail.com>
2021-03-24 23:50:57 -0400

    Adv JS - Finished Sec 10

---
+++ Javascript/Advanced Javascript (Udemy. Asim Hussain)/Section 9. Networking/jasonp.js
@@ -0,0 +1,16 @@
+// Regular json.
+[
+    {
+        "id": 1,
+        "first_name": "Asim",
+        "last_name": "Hussain"
+    }
+]
+
+asimFunction( // Valid javascript. No longer json.
+    [{
+        "id": 1,
+        "first_name": "Asim",
+        "last_name": "Hussain"
+    }]
+);
