260dcb4517dd468dcd81a9f6682e4a701a95a3af
h-cheema <harjinder.sc96@gmail.com>
2021-04-08 22:00:22 -0400

    JS DOM 2021 Part 2 - Finished lecture 47/127

--- WebDev/Javascript/DOM 2021 Build Dynamic Websites with JavaScript Part 2 (Udemy. Clyde Matthew)/Section 2. Different types of events/45. Form building/app.js
+++ WebDev/Javascript/DOM 2021 Build Dynamic Websites with JavaScript Part 2 (Udemy. Clyde Matthew)/Section 2. Different types of events/45. Form building/app.js
@@ -1,8 +1,29 @@
-// grab out output <p> element
-
+// grab out output <p> element.
 let output = document.getElementById('output');
+
+
+// grab out input textbox.
+let nameText = document.querySelector('input[type=text]');
+
 
 document.querySelector('form').addEventListener('submit', (e) => {
     output.textContent = `A submit event has been fired at ${e.timeStamp}`;
-    e.preventDefault()
+    e.preventDefault();
+});
+
+
+// lets play around with different event types.
+nameText.addEventListener('input', () => {
+    output.textContent = `The value entered is ${nameText.value}`;
+});
+
+
+nameText.addEventListener('focus', (e) => {
+    e.target.style.background = "lightblue";
+    e.target.style.color = "red";
+});
+
+
+nameText.addEventListener('copy', () => {
+    alert('you have copied text');
 });
