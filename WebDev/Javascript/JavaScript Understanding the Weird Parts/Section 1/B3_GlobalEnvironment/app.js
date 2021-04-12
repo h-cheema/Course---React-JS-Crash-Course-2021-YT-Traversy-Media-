137e1ab66edd9c107726478a897a5acf81ad486e
h-cheema <harjinder.sc96@gmail.com>
2021-04-12 13:21:41 -0400

    JavaScript UTWP - Finished lecture 14/85

--- WebDev/Javascript/JavaScript Understanding the Weird Parts/Section 1/B3_GlobalEnvironment/app.js
+++ WebDev/Javascript/JavaScript Understanding the Weird Parts/Section 1/B3_GlobalEnvironment/app.js
@@ -1,8 +1,21 @@
-b();
-console.log(a)
+// b();
+// console.log(a)
+// var a = 'Hello World!';
+// function b() { 
+//     console.log('Called b');
+// }
 
-var a = 'Hello World!';
 
-function b() { 
-    console.log('Called b');
-}
+
+// a = undefined; // Manual way to set undefined. Hard to know if you or js set it.
+// var a; // Always better to let js handle it like this.
+// console.log(a)
+
+// if (a === undefined) {
+//     console.log('a is undefined.');
+// } else {
+//     console.log('a is defined.');
+// }
+
+
+
