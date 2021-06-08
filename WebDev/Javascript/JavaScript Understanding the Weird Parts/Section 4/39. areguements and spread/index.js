fd33ff4c0f1afbf61bf7c5e0a3ecc276c373a222
h-cheema <harjinder.sc96@gmail.com>
2021-06-07 23:52:19 -0400

    JavaScript UTWP - Finished lecture 4-39/85

---
+++ WebDev/Javascript/JavaScript Understanding the Weird Parts/Section 4/39. areguements and spread/index.js
@@ -0,0 +1,14 @@
+function greet(fname, lname, lang, ...other) { // ...other is extra parameters.
+    
+    lang = lang || 'en' // Default parameter
+
+    if (arguments.length === 0) { // arguments is a key word
+        console.log('missing parameters')
+    } else {
+        console.log('arg 0' + arguements[0])
+    }
+
+
+
+
+}
