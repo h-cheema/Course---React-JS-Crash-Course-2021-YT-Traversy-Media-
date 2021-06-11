134490f3c7567092e834f6292a7acec48f743460
h-cheema <harjinder.sc96@gmail.com>
2021-06-10 23:55:49 -0400

    JavaScript UTWP - Finished lecture 4-50/85

---
+++ WebDev/Javascript/JavaScript Understanding the Weird Parts/Section 4/48. function factories/index.js
@@ -0,0 +1,19 @@
+function makeGreeting(language) {
+    return function(firstName, lastName) {
+
+        if (language === 'en') {
+            console.log('Hello ' + firstName + ' ' + lastName)
+        }
+
+        if (language === 'es') {
+            console.log('Hello ' + firstName + ' ' + lastName)
+        }
+
+    }
+}
+
+
+// These functions both point to different spots in memeory.
+// Each function has its own exe context every time makeGreeting() is called
+var greetEnglish = makeGreeting('en'); // Closure points to language = en
+var greetSpanish = makeGreeting('es'); // Closure points to language = es
